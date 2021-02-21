85e6159fcd55ce3eed02af0637294d75ce75d1a0
El Mehdi Lebbar <38670953+ElMehdiLebbar@users.noreply.github.com>
2021-02-21 16:53:33 +0100

    Add files via upload

---
+++ Hello/js/main.js
@@ -0,0 +1,10 @@
+//Just a demonstration for Seule
+let app = new Seule('#app')
+
+app
+    .find('button')
+    .click((e)=> e.text('Hello World'))
+
+//You may remove-it !
+
+/* Your Js Code goes here ! */
